73e57b17063e095061bb34f3e263525a46c655f1
Umbusk <moises@umbusk.com>
2025-11-10 08:58:56 -0400

    Update public-v2.js

--- reportes/js/public-v2.js
+++ reportes/js/public-v2.js
@@ -554,7 +554,7 @@ async function exportToWord() {
         ${images.countries ? `
         <div class="section">
             <h2>🌍 Distribución por País</h2>
-            <img src="${images.countries}" class="chart-img" width="280">
+            <img src="${images.countries}" class="chart-img" width="460">
         </div>
         ` : ''}
         
